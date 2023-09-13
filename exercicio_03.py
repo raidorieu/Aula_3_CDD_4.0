473a61c188c7f2a1fb82d56a8af633e42882191b
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-09-13 10:56:59 -0300

    Fiz o terceiro exercício

--- exercicio_03.py
+++ exercicio_03.py
@@ -1,28 +1,28 @@
 #que mês estamos
-n=int(input("Digite um número, que lhe direi o mês: "))
+n=int(input("何月: "))
 if n>12 or n<=0:
-    print("dado inválido.")
+    print("無効なデータ")
 if n==1:
-    print("janeiro")
+    print("一月")
 if n==2:
-    print("fevereiro")
+    print("二月")
 if n==3:
-    print("março")
+    print("三月")
 if n==4:
-    print("abril")
+    print("四月")
 if n==5:
-    print("maio")
+    print("五月")
 if n==6:
-    print("junho")
+    print("六月")
 if n==7:
-    print("julho")
+    print("七月")
 if n==8:
-    print("agosto")
+    print("八月")
 if n==9:
-    print("setembro")
+    print("九月")
 if n==10:
-    print("outubro")
+    print("十月")
 if n==11:
-    print("novembro")
+    print("十一月")
 if n==12:
-    print("dezembro")
+    print("十二月")
