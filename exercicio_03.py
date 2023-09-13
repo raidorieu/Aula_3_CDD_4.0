2cccd677c414ebb4e3fbba3977d09f890ff9c70b
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-09-13 11:09:43 -0300

    Fiz o terceiro exercício

--- exercicio_03.py
+++ exercicio_03.py
@@ -2,27 +2,39 @@
 n=int(input("何月: "))
 if n>12 or n<=0:
     print("無効なデータ")
+
 if n==1:
     print("一月")
-if n==2:
-    print("二月")
-if n==3:
-    print("三月")
-if n==4:
-    print("四月")
-if n==5:
-    print("五月")
-if n==6:
-    print("六月")
-if n==7:
-    print("七月")
-if n==8:
-    print("八月")
-if n==9:
-    print("九月")
-if n==10:
-    print("十月")
-if n==11:
-    print("十一月")
-if n==12:
-    print("十二月")
+else:
+    if n==2:
+        print("二月")
+    else:
+        if n==3:
+            print("三月")
+        else:
+            if n==4:
+                print("四月")
+            else:
+                if n==5:
+                    print("五月")
+                else:
+                    if n==6:
+                        print("六月")
+                    else:
+                        if n==7:
+                            print("七月")
+                        else:
+                            if n==8:
+                                print("八月")
+                            else:
+                                if n==9:
+                                    print("九月")
+                                else:
+                                    if n==10:
+                                        print("十月")
+                                    else:
+                                        if n==11:
+                                            print("十一月")
+                                        else:
+                                            if n==12:
+                                                print("十二月")
