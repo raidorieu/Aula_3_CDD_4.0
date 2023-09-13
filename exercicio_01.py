b5729dbe8d91d4be46cc7f89492240d754680e08
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-09-13 08:28:37 -0300

    Fiz o primeiro exercício

---
+++ exercicio_01.py
@@ -0,0 +1,7 @@
+#par ou impar
+#Na segunda aula, realizei um exercício igual. Acabei pulando algumas casinhas, mas tá tudo certo kkkk
+n=float(input("Digite um número: "))
+if n%2==0:
+    print("O número é par")
+else:
+    print("O número é impar")
