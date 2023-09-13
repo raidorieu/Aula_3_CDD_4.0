a7a79626b5c156bf01aa58e95eefc30c1ea38e2d
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-09-13 10:40:07 -0300

    Fiz o terceiro exercício

---
+++ exercicio_03.py
@@ -0,0 +1,28 @@
+#que mês estamos
+n=int(input("Digite um número, que lhe direi o mês: "))
+if n>12 or n<=0:
+    print("dado inválido.")
+if n==1:
+    print("janeiro")
+if n==2:
+    print("fevereiro")
+if n==3:
+    print("março")
+if n==4:
+    print("abril")
+if n==5:
+    print("maio")
+if n==6:
+    print("junho")
+if n==7:
+    print("julho")
+if n==8:
+    print("agosto")
+if n==9:
+    print("setembro")
+if n==10:
+    print("outubro")
+if n==11:
+    print("novembro")
+if n==12:
+    print("dezembro")
